5d8e3a92d60ddb901f70de630194041c722c6801
emily <limeyb7@gmail.com>
2017-08-23 15:48:22 +0100

    migrate skills data to root dir

---
+++ data.js
@@ -0,0 +1,34 @@
+const LANGUAGES = "languages";
+const FRONTEND_LIBRARIES = "frontend libraries";
+const FRONTEND_TOOLING = "frontend tooling";
+
+exports.skills = [
+  {
+    type: LANGUAGES,
+    items: [
+      "HTML",
+      "CSS",
+      "JavaScript (ES6)",
+      "Node.js",
+      "SQL",
+      "Pug",
+      "Markdown"
+    ]
+  },
+  {
+    type: FRONTEND_LIBRARIES,
+    items: [
+      "React",
+      "Redux",
+      "React router",
+      "Socket.io",
+      "Leaflet.js",
+      "Chart.js",
+      "jQuery"
+    ]
+  },
+  {
+    type: FRONTEND_TOOLING,
+    items: ["Webpack", "Babel", "Gulp", "Sass", "Browserify"]
+  }
+];
--- pug.js
+++ pug.js
@@ -1,8 +1,9 @@
 const pug = require("pug");
 const fs = require("fs");
+const locals = require("./data.js");
 
 const filePath = "templates/base.pug";
 
-const html = pug.renderFile(filePath);
+const html = pug.renderFile(filePath, locals);
 
 fs.writeFileSync("./pug.html", html);
