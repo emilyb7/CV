3dad4ca5063dfaa13bf919e43de2161f095dd526
emily <limeyb7@gmail.com>
2017-08-23 16:07:59 +0100

    add new skills data

--- data.js
+++ data.js
@@ -1,6 +1,10 @@
 const LANGUAGES = "languages";
 const FRONTEND_LIBRARIES = "frontend libraries";
 const FRONTEND_TOOLING = "frontend tooling";
+const BACKEND = "backend";
+const TESTING_CI = "testing and CI";
+const TOOLS = "tools";
+const OTHER = "other";
 
 exports.skills = [
   {
@@ -30,5 +34,30 @@ exports.skills = [
   {
     type: FRONTEND_TOOLING,
     items: ["Webpack", "Babel", "Gulp", "Sass", "Browserify"]
+  },
+  {
+    type: BACKEND,
+    items: [
+      "Express",
+      "hapi",
+      "keystone.js",
+      "Socket.io",
+      "PostgreSQL",
+      "MongoDB",
+      "CouchDB",
+      "Redis"
+    ]
+  },
+  {
+    type: TESTING_CI,
+    items: ["Jest", "Enzyme", "Tape", "QUnit", "Travis CI"]
+  },
+  {
+    type: TOOLS,
+    items: ["Git", "GitHub", "NPM", "Heroku", "JIRA", "Trello"]
+  },
+  {
+    type: OTHER,
+    items: ["TDD", "UX", "Wireframes", "Agile (Scrum)", "Pair programming"]
   }
 ];
